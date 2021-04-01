9c902bada92bc767115cd0e726269049a88e3a3c
MalinaMarin <79064186+MalinaMarin@users.noreply.github.com>
2021-04-01 15:55:09 +0300

    added price prediction submodule

---
+++ HousePricePrediction-AI/HousingPricePredictionProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using HousingPricePredictionProjectML.Model;
+namespace HousingPricePredictionProject
+{
+    class Program
+    {
+        // Function for printing the predicted price, given the house's features
+        static void showPredictedPrice(string[] features)
+        {
+            ModelInput sampleData = new ModelInput()
+            {
+                Tip_proprietate = features[0],
+                Nr_camere = int.Parse(features[1]),
+                Suprafata = float.Parse(features[2]),
+                Suprafata_teren = float.Parse(features[3]),
+                An_constructie = int.Parse(features[4]),
+                Zona = features[5],
+            };
+
+            var predictionResult = ConsumeModel.Predict(sampleData);
+            Console.WriteLine($"\n\nPredicted Price: {predictionResult.Score}\n\n");
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            string[] newHouse = new string[6] { "CAS", "6", "554", "968", "2000", "tatarasi" };
+            showPredictedPrice(newHouse);
+        }
+    }
+}
