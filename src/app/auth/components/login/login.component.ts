d1c077a727827216c13a12544c1fe29549336be5
AdelinChelariu <adelinchelariu@gmail.com>
2021-04-07 23:03:16 +0300

    Login template added

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { LoginPayload } from 'src/app/auth/models/auth.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public hide = true;
+  login: FormGroup;
+  email: String;
+  password: String;
+  payload: LoginPayload={
+    email:'',
+    password:''
+  };
+
+  constructor(private form: FormBuilder) { }
+
 
   ngOnInit(): void {
+    this.login=this.form.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]]
+      }
+    )
   }
-
+  onSubmit() {
+    this.payload.email = this.login.get('email').value;
+    this.payload.password = this.login.get('password').value;
+    console.log(this.payload);
+  }
 }
