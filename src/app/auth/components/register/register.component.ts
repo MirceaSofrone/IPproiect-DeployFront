fdffd0aa215420855b2756db108648119d71eb06
Boris Pavel <boris.pavel37@gmail.com>
2021-04-07 15:24:06 +0300

    added register

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public hide = true;
+  public hideConfirm = true;
+  registerForm: FormGroup;
+  firstName: string;
+  lastName: string;
+  emailAddress: string;
+  phoneNumber: string;
+  passwd: string;
+  confirmPasswd: string;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      emailAddress: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+      passwd: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPasswd: ['', [Validators.required, Validators.minLength(8)]]
+    }
+    )
+  }
+
+  ngOnSubmit() {
+    if (this.registerForm.get('passwd').value !== this.registerForm.get("confirmPasswd").value) {
+      alert("Passwords must match!")
+    }
+    else {
+      this.passwd = this.registerForm.get('passwd').value
+      this.confirmPasswd = this.registerForm.get('confirmPasswd').value
+    }
   }
 
 }
