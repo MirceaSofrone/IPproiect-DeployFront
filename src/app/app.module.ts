2c99864e66bca4d8ac9d17a36b6559c568b18115
Andreea Higiu <higiuandreea@gmail.com>
2021-04-15 20:51:43 +0300

    added gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HouseGalleryComponent } from './house-gallery/house-gallery.component';
+import { UserContactComponent } from './user-contact/user-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HouseGalleryComponent,
+    UserContactComponent
   ],
   imports: [
     BrowserModule,
