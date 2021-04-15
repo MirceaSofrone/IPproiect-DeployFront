bd095ec232c12f5d2df1e1aafe6d29ec1b16a618
Dragos CIOPEI <dciopei@ias.bitdefender.biz>
2021-04-15 13:51:33 +0300

    Modul AI integrat cu API-ul via body

---
+++ PricePrediction/WebAPI/Controllers/PriceController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HousingPricePredML;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PriceController : ControllerBase
+    {
+        [HttpGet]
+        public Output Get(HousingPricePredML.Model.ModelInput InputData)
+        {
+            Output OutputProperty = new Output()
+            {
+                TipProprietate = InputData.TipProprietate,
+                NrCamere = InputData.NrCamere,
+                Suprafata = InputData.Suprafata,
+                SuprafataTeren = InputData.SuprafataTeren,
+                AnConstructie = InputData.AnConstructie,
+                Zona = InputData.Zona,
+                Pret = HousingPricePredML.Model.ConsumeModel.SendPredictedPrice(InputData)
+            };
+
+            return OutputProperty;
+        }
+    }
+}
+
---
+++ PricePrediction/WebAPI/Output.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI
+{
+    public class Output
+    {
+        public string TipProprietate { get; set; }
+
+        public float NrCamere { get; set; }
+
+        public float Suprafata { get; set; }
+
+        public float SuprafataTeren { get; set; }
+
+        public float AnConstructie { get; set; }
+
+        public string Zona { get; set; }
+
+        public float Pret { get; set; }
+    }
+}
