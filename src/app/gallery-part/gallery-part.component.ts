6171a5cb394f450b1f6108e6f03325372461a919
ionut.gr24 <ionut.gr24@yahoo.com>
2021-04-17 19:46:35 +0300

    fixed merge

--- src/app/gallery-part/gallery-part.component.ts
+++ src/app/gallery-part/gallery-part.component.ts
@@ -9,7 +9,7 @@ import {NgxGalleryAnimation} from '@kolkov/ngx-gallery';
 })
 
 export class GalleryPartComponent implements OnInit {
- 
+
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
 
@@ -40,30 +40,30 @@ export class GalleryPartComponent implements OnInit {
     }
   ];
 
-  this.galleryImages = [
+    this.galleryImages = [
     {
-      small: "../../assets/1.jpg",
-      medium: "../../assets/1.jpg",
-      big: "../../assets/1.jpg"
+      small: '../../assets/1.jpg',
+      medium: '../../assets/1.jpg',
+      big: '../../assets/1.jpg'
     },
     {
-      small: "../../assets/2.jpg",
-      medium: "../../assets/2.jpg",
-      big: "../../assets/2.jpg"
+      small: '../../assets/2.jpg',
+      medium: '../../assets/2.jpg',
+      big: '../../assets/2.jpg'
     },
     {
-      small: "../../assets/3.jpg",
-      medium: "../../assets/3.jpg",
-      big: "../../assets/3.jpg"
-    },{
-      small: "../../assets/4.jpg",
-      medium: "../../assets/4.jpg",
-      big: "../../assets/4.jpg"
+      small: '../../assets/3.jpg',
+      medium: '../../assets/3.jpg',
+      big: '../../assets/3.jpg'
+    }, {
+      small: '../../assets/4.jpg',
+      medium: '../../assets/4.jpg',
+      big: '../../assets/4.jpg'
     },
     {
-      small: "../../assets/5.jpg",
-      medium: "../../assets/5.jpg",
-      big: "../../assets/5.jpg"
+      small: '../../assets/5.jpg',
+      medium: '../../assets/5.jpg',
+      big: '../../assets/5.jpg'
     }
   ];
   }
