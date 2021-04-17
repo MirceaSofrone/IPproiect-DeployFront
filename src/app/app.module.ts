5cbea76477f8ab0c03725f80cd6b629c83fb9c6a
ionut.gr24 <ionut.gr24@yahoo.com>
2021-04-17 19:06:49 +0300

    added user-contact email service mockup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HouseGalleryComponent } from './house-gallery/house-gallery.component';
 import { UserContactComponent } from './user-contact/user-contact.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +14,9 @@ import { UserContactComponent } from './user-contact/user-contact.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user-contact/user-contact.component.ts
+++ src/app/user-contact/user-contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-user-contact',
   templateUrl: './user-contact.component.html',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient ) { }
 
   ngOnInit(): void {
   }
+  // tslint:disable-next-line:typedef
+   onSubmit(data){
+   this.http.post('https://607af93abd56a60017ba3474.mockapi.io/api/v1/emails', data.form.value)
+     .subscribe((result) => {
+       console.warn('result', result);
+     });
+   console.warn(data.form.value);
 
+  }
 }
