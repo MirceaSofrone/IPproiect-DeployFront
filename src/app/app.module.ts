c808cbc01e18c8e24a38f45947f47b26e4ed7279
RomilaIngridMaria <62561385+RomilaIngridMaria@users.noreply.github.com>
2021-04-22 14:54:22 +0300

    house-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GalleryPartComponent } from './gallery-part/gallery-part.component';
+import {UserContactComponent} from './user-contact/user-contact.component';
+import {HouseInfoComponent} from './house-info/house-info.component';
+import {MapComponent} from './map/map.component';
+import { PhotoCarouselComponent } from './photo-carousel/photo-carousel.component';
+
+import { AgmCoreModule } from '@agm/core';
+import { HttpClientModule} from '@angular/common/http';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery'; 
+import { HammerModule } from '@angular/platform-browser'; 
+import {IvyCarouselModule} from 'angular-responsive-carousel';
+import {SwiperModule} from 'swiper/angular';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryPartComponent,
+    UserContactComponent,
+    HouseInfoComponent,
+    MapComponent,
+    PhotoCarouselComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    AppRoutingModule,
+    HttpClientModule, 
+    NgxGalleryModule , 
+    HammerModule,
+    IvyCarouselModule,
+    SwiperModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBBhqL0VAJqQpXMATFPcLdsq9RYV0kBjTQ',
+     
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/gallery-part/gallery-part.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import {NgxGalleryOptions} from '@kolkov/ngx-gallery';
+import {NgxGalleryImage} from '@kolkov/ngx-gallery';
+import {NgxGalleryAnimation} from '@kolkov/ngx-gallery';
+
+@Component({
+  selector: 'app-gallery-part',
+  templateUrl: './gallery-part.component.html',
+  styleUrls: ['./gallery-part.component.css']
+})
+
+export class GalleryPartComponent implements OnInit {
+ 
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.galleryOptions = [
+    {
+      width: '45vw',
+      height: '40vw',
+      thumbnailsColumns: 4,
+      imageAnimation: NgxGalleryAnimation.Slide
+    },
+    {
+      breakpoint: 1110,
+      width: '100%',
+      height: '60vw',
+      imagePercent: 80,
+      thumbnailsPercent: 20,
+      thumbnailsMargin: 20,
+      thumbnailMargin: 20
+    },
+    // max-width 800
+    {
+      breakpoint: 800,
+      width: '100%',
+      height: '80vw',
+      imagePercent: 80,
+      thumbnailsPercent: 20,
+      thumbnailsMargin: 5,
+      thumbnailMargin: 5
+    },
+    // max-width 400
+    {
+      breakpoint: 400,
+      preview: false
+    }
+  ];
+  
+
+  this.galleryImages = [
+    {
+      small: "https://picsum.photos/id/0/5616/3744",
+      medium: "https://picsum.photos/id/0/5616/3744",
+      big: "https://picsum.photos/id/0/5616/3744"
+    },
+    {
+      small: "../../assets/2.jpg",
+      medium: "../../assets/2.jpg",
+      big: "../../assets/2.jpg"
+    },
+    {
+      small: "../../assets/3.jpg",
+      medium: "../../assets/3.jpg",
+      big: "../../assets/3.jpg"
+    },{
+      small: "../../assets/4.jpg",
+      medium: "../../assets/4.jpg",
+      big: "../../assets/4.jpg"
+    },
+    {
+      small: "../../assets/5.jpg",
+      medium: "../../assets/5.jpg",
+      big: "../../assets/5.jpg"
+    }
+  ];
+  }
+}
