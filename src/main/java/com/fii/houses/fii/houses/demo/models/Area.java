214a887ad40ad78db2f74680357fb05c8248110e
Raluca Macovei <ralumacovei@yahoo.com>
2021-05-11 23:36:23 +0300

    Update Area.java

--- src/main/java/com/fii/houses/fii/houses/demo/models/Area.java
+++ src/main/java/com/fii/houses/fii/houses/demo/models/Area.java
@@ -8,7 +8,7 @@ public class Area {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
 //    @Column(columnDefinition = "BINARY(16)")
-    private UUID areaID;
+    private Integer areaID;
     private String area;
     private double latitudeMax;
     private double latitudeMin;
