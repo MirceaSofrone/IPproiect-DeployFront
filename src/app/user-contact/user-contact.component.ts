c378713d1df05935c803482ef961f0edf656df4e
ionut.gr24 <ionut.gr24@yahoo.com>
2021-05-19 12:36:15 +0300

    nothing

--- src/app/user-contact/user-contact.component.ts
+++ src/app/user-contact/user-contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 @Component({
   selector: 'app-user-contact',
   templateUrl: './user-contact.component.html',
@@ -13,11 +13,34 @@ export class UserContactComponent implements OnInit {
   }
   // tslint:disable-next-line:typedef
    onSubmit(data){
-   this.http.post('https://607af93abd56a60017ba3474.mockapi.io/api/v1/emails', data.form.value)
+
+    const bearer = localStorage.getItem('bearer');
+    const token = `Bearer ${bearer}`;
+    console.log(token);
+    // const httpHeaders = new HttpHeaders({
+    //    'Content-Type': 'application/json',
+    //   'Access-Control-Allow-Origin': '*',
+    //    Authorization: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMi82WjdsUjVLb3JqY3FuOGhVNEVOMGxiUFJGY3lWSUFyOXowWFJqa29NPSIsImlhdCI6MTYyMDgyNTY3NSwiZXhwIjoxNjIwOTEyMDc1fQ.dAZY5UoGuCWg7R5VMJvb1qxGiQJgbfeR3ptXWMz-srhCU6oLTquonAWyvnkJF5Uw1CX_rkMQfFkj9MCHG3VJvA'
+    //  });
+    const httpHeaders = new HttpHeaders();
+    httpHeaders.append('content-type', 'application/json');
+    httpHeaders.append('Authorization', token);
+    const message = data.form.value.message;
+
+    console.log(httpHeaders);
+    const userEmail = localStorage.getItem('userEmail');
+    const sellerEmail = localStorage.getItem('sellerEmail');
+    const sendData = {
+      emailFrom : userEmail,
+      emailTo : sellerEmail,
+      message
+    };
+    console.log(sendData);
+    this.http.post('https://hpp-auth.herokuapp.com/api/v1/feedback', sendData, {headers: httpHeaders })
      .subscribe((result) => {
        console.warn('result', result);
      });
-   console.warn(data.form.value);
+
 
   }
 }
