1990bd507ef3fc83efa423083ccd7218034e2c05
MirceaSofrone <mircea.sofrone@gmail.com>
2021-05-25 16:55:23 +0300

    7th try

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
+app.use(express.static(__dirname + '/dist/ipproiect'));
+app.listen(process.env.PORT || 8080);
 
-app.use(express.static(__dirname + '/dist/IPFront'));
-
-app.get('/*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/dictionary/index.html'));
-});
-
-
-app.listen(process.env.PORT || 8000);
+app.get('/*', function(req,res){
+  const fullPath = path.join(__dirname + '/dist/ipproiect/index.html');
+  res.sendFile(fullPath);
+})
