5538ed72d976400d3f03e577b7f9bec2539120cb
MirceaSofrone <mircea.sofrone@gmail.com>
2021-05-25 16:09:04 +0300

    deploy 5th try

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/ipproiect'));
+app.use(express.static(__dirname + './dist'));
 
 app.get('/*', (req, res) => 
-    res.sendFile('index.html', {root: 'dist/ipproiect'}),
+    res.sendFile(path.join(__dirname + '/dist/index.html'))
 );
 
 app.listen(process.env.PORT || 8080);
