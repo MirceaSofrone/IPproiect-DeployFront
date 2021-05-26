02289aac8d2e5b6e2815b838adc5fbe75d0568e8
Dragos CIOPEI <dciopei@ias.bitdefender.biz>
2021-05-26 23:19:12 +0300

    css fixes

--- src/app/forum/service/user-service.service.ts
+++ src/app/forum/service/user-service.service.ts
@@ -9,15 +9,12 @@ export class UserService {
 
   public getID(): number {
     let usertoken = localStorage.getItem("userID");
-    //return usertoken;
-    //TODO: get from local storage
-    return 26;
+    return parseInt(usertoken);
   }
 
   public getToken(): string {
-    
-   // return localStorage.getItem("token");
-   return "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0MUVYa3hieVA0aDVvNk5BQ0VTL2hhWUxndzE1Rm1LdDM5SnFZelhmSHlVPSIsImlhdCI6MTYyMjAxODc1MCwiZXhwIjoxNjIyMTA1MTUwfQ.SDTYrHnL9mDgwTqxCtVD_lgfeEb1jcCBZV4REuVIWEqJR0svW2TCQCaOAsea26qTIdp0WGmk-mIKuqsXuloOZw";
+  
+   return ("Bearer " + localStorage.getItem("token"));
   }
 }
 
